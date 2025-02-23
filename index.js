13ac0d0a5ef7536578645ded67b873d77d753f38
barahkh <barahkh2005@icloud.com>
2025-02-23 20:17:00 +0300

    it's a order

---
+++ index.js
@@ -0,0 +1,35 @@
+let username = "Barah";         //اذا بدي اعرف اسم 
+let x = 5;
+console.log("Hello world");     //اذا بدي اطبع في java script
+console.log(x);
+//كيف بدي اعمل fun
+function printHello(){
+    console.log("Hello");
+}
+
+printHello();                //بدي استدعي هاي ال fun
+
+let string = "barah";        //كيف بدي انشئ string 
+console.log(string.includes("m"));
+if(string.includes("r")){                            //كيف بدي انشئ if
+    console.log("char r is not found");}
+    else{
+    console.log("char r is not found");
+
+    }
+
+    //كيف بدي انشئ Array
+    let varuable = ["1","2","5"];  //يقدر اضيف قيم قد ما بدي 
+    console.log(varuable[2]);
+
+    //classمثلا بدي انشئ 
+    let names = {name1 : "barah" , name2:sami  };
+    console.log(names);
+    //اذا بدي اطبع اسم معين 
+
+    console.log(names.name1);
+    console.log(names["name2"]);
+
+
+
+
